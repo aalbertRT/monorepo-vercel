60c17029bef7361b9e2bfc81fdb8fc0843138a6f
Aurélien Albert <aurelienalbert.dev@gmail.com>
2024-06-09 21:34:18 +0200

    Add CORS for local test

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 from blueprints.edge import edge
 
 app = Flask(__name__)
+app.config.from_prefixed_env()
+
+if app.config.get("MODE") == "staging":
+    CORS(app)
 
 app.register_blueprint(edge, url_prefix="/api")
