44816dc3c9a436f9c0bc4fa4e37d7164a8afcc77
Aurélien Albert <aurelienalbert.dev@gmail.com>
2024-06-10 21:41:31 +0200

    save

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 
-from blueprints.edge import edge
-
 app = Flask(__name__)
 app.config.from_prefixed_env()
 
 if app.config.get("MODE") == "staging":
     CORS(app)
 
-app.register_blueprint(edge, url_prefix="/api")
+@app.route("")
+def index():
+    return {"message": "Hello Mr Kennedy, you're hitting the api"}
--- backend/blueprints/edge.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-edge = Blueprint("edge", __name__)
-
-@edge.route("")
-def index():
-    return {"message": "Hello Mr Kennedy, you're hitting the api"}
